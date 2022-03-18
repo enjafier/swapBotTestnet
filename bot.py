bd5ae3b9028e9ee9b82676c56f34e339d0a6b3eb
Hafiznur <48821495+enjafier@users.noreply.github.com>
2022-03-18 23:46:20 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -63,8 +63,8 @@ while True:
         balance = w3.eth.get_balance(w3.eth.default_account)
         timestamp = w3.eth.getBlock('latest').timestamp
         deadline = timestamp + 60 * 30
-        avax_amount = w3.toWei(random.uniform(0.2,0.3), 'ether')
-        usdc_amount = w3.toWei(random.uniform(0.2,0.3), 'ether')
+        avax_amount = w3.toWei(random.uniform(0.1,0.2), 'ether')
+        usdc_amount = w3.toWei(random.uniform(0.1,0.2), 'ether')
         if balance >= w3.toWei(1.4, 'ether'):
             tx = avax_usdc(avax_amount, deadline)
             print("Swapping {} AVAX for {} USDC".format(w3.fromWei(avax_amount,'ether'), w3.fromWei(usdc_amount,'ether')))
